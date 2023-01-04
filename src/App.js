87f08d5ca300f999ad18c0075587375acdbff5a5
Madhuri <madhuribansode12@gmail.com>
2023-01-04 17:17:50 +0530

    style page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Navbar/>
+      <div className='Main'>
+      <div className="Container" >
+       <div className='title'>Consolidate your restaurants' insights in one place. Unlock growth.</div>
+       <div className='subtitle'>Your one stop solution to view mission-critical data and insights from all your locations in one place.</div>
+
+      </div>
+      <div className='image_container'>
+        <img id='img1' src='https://voosh.in/static/media/my_profile.61da110e7e1e80c97356.webp' alt=''/>
+      </div>
+      </div>
+      <div className='booknow'>
+
+      </div>
     </div>
   );
 }
